3a8ffb9d83672a5481b3882b37f8f27cf55ca59b
Eric MacAdie <Eric@MacAdie.net>
2013-04-21 23:29:25 -0500

    I added DNSSECWithProviderSpockTest

---
+++ src/test/groovy/info/shelfunit/DNS/success/DNSSECWithProviderSpockTest.groovy
@@ -0,0 +1,50 @@
+package info.shelfunit.DNS
+
+import  org.xbill.DNS.*
+
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.Security
+import java.security.Signature
+
+import spock.lang.Specification
+
+import org.xbill.DNS.DNSSEC.Algorithm
+
+public class DNSSECWithProviderSpockTest extends Specification {
+  
+  private static final String SIGNATURE_ALGORITHM = "SHA1withRSA"
+  private static final String KEY_ALGORITHM = "RSA"
+  int algorithm = Algorithm.RSASHA1
+  byte[] toSign = "The quick brown fox jumped over the lazy dog.".getBytes()
+  def mga = new MyGroovyAssert()
+
+
+  public void setup() { }
+
+  public void tearDown() {
+  }
+
+  def "testSignSoftware"() throws Exception {
+
+    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KEY_ALGORITHM)
+    keyPairGenerator.initialize(512)
+    KeyPair keyPair = keyPairGenerator.generateKeyPair()
+
+    Signature signer = Signature.getInstance(SIGNATURE_ALGORITHM)
+    signer.initSign(keyPair.getPrivate())
+    signer.update(toSign)
+    byte[] signature = signer.sign()
+    expect: mga.notNull(signature)
+
+    // verify the signature
+    when:
+    Signature verifier = Signature.getInstance(SIGNATURE_ALGORITHM)
+    verifier.initVerify(keyPair.getPublic())
+    verifier.update(toSign)
+    boolean verify = verifier.verify(signature)
+    then:
+    mga.that(verify)
+
+  }
+}
