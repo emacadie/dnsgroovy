f3f5ca6874fc1a52763577370056bba97ab96ca0
Eric MacAdie <Eric@MacAdie.net>
2013-04-22 21:17:05 -0500

    I added GPOSRecordSpockTest.groovy

--- src/test/groovy/info/shelfunit/DNS/success/GPOSRecordSpockTest.groovy
+++ src/test/groovy/info/shelfunit/DNS/success/GPOSRecordSpockTest.groovy
@@ -45,6 +45,10 @@ public class GPOSRecordSpockTest extends Specification {
     def mgu = new MyGroovyUtil()
     def mga = new MyGroovyAssert()
 
+    private Name	m_n
+    private long	m_ttl
+    private double	m_lat, m_long, m_alt
+
     def "test_ctor_0arg"() {
 	GPOSRecord gr = new GPOSRecord()
 	expect:
@@ -60,26 +64,22 @@ public class GPOSRecordSpockTest extends Specification {
 	expect:
 	mga.that(r instanceof GPOSRecord)
     }
-    /*
-    public static class Test_Ctor_6arg_doubles extends TestCase
-    {
-	private Name	m_n
-	private long	m_ttl
-	private double	m_lat, m_long, m_alt
+    //////////////////////////////////////
 
-	protected void setUp() throws TextParseException
-	{
-	    m_n = Name.fromString("The.Name.")
+
+    protected void setUpTest_Ctor_6arg_doubles() throws TextParseException {
+	m_n = Name.fromString("The.Name.")
 	    m_ttl = 0xABCDL
 	    m_lat = -10.43
 	    m_long = 76.12
 	    m_alt = 100.101
-	}
+    }
 	
-	public void test_basic() throws TextParseException
-	{
+	def "test_basic_Ctor_6arg_doubles"() throws TextParseException {
+	    setUpTest_Ctor_6arg_doubles()
 	    GPOSRecord gr = new GPOSRecord(m_n, DClass.IN, m_ttl,
 					   m_long, m_lat, m_alt)
+	    expect:
 	    mgu.equals(m_n, gr.getName())
 	    mgu.equals(DClass.IN, gr.getDClass())
 	    mgu.equals(Type.GPOS, gr.getType())
@@ -91,67 +91,53 @@ public class GPOSRecordSpockTest extends Specification {
 	    mgu.equals(new Double(m_lat).toString(), gr.getLatitudeString())
 	    mgu.equals(new Double(m_alt).toString(), gr.getAltitudeString())
 	}
-
-	public void test_toosmall_longitude() throws TextParseException
-	{
-	    try {
-		new GPOSRecord(m_n, DClass.IN, m_ttl,
-			       -90.001, m_lat, m_alt)
-		fail("IllegalArgumentException not thrown")
-	    }
-	    catch(IllegalArgumentException e){}
-	}
-
-	public void test_toobig_longitude() throws TextParseException
-	{
-	    try {
-		new GPOSRecord(m_n, DClass.IN, m_ttl,
-			       90.001, m_lat, m_alt)
-		fail("IllegalArgumentException not thrown")
-	    }
-	    catch(IllegalArgumentException e){}
-	}
-
-	public void test_toosmall_latitude() throws TextParseException
-	{
-	    try {
-		new GPOSRecord(m_n, DClass.IN, m_ttl,
-			       m_long, -180.001, m_alt)
-		fail("IllegalArgumentException not thrown")
-	    }
-	    catch(IllegalArgumentException e){}
-	}
-
-	public void test_toobig_latitude() throws TextParseException
-	{
-	    try {
-		new GPOSRecord(m_n, DClass.IN, m_ttl,
-			       m_long, 180.001, m_alt)
-		fail("IllegalArgumentException not thrown")
-	    }
-	    catch(IllegalArgumentException e){}
-	}
-
-	public void test_invalid_string()
-	{
-	    try {
-		new GPOSRecord(m_n, DClass.IN, m_ttl,
-			       new Double(m_long).toString(),
-			       "120.\\00ABC", new Double(m_alt).toString())
-		fail("IllegalArgumentException not thrown")
-	    }
-	    catch(IllegalArgumentException e){}
-	}
+    
+    def "test_toosmall_longitude_Ctor_6arg_doubles"() throws TextParseException {
+	setUpTest_Ctor_6arg_doubles()
+	    when:
+		new GPOSRecord(m_n, DClass.IN, m_ttl, -90.001, m_lat, m_alt)
+	    then:
+	    thrown(IllegalArgumentException.class )
     }
-    */
-    /*
-    public static class Test_Ctor_6arg_Strings extends TestCase
-    {
-	private Name	m_n
-	private long	m_ttl
-	private double	m_lat, m_long, m_alt
 
-	protected void setUp() throws TextParseException
+    def "test_toobig_longitude_Ctor_6arg_doubles"() throws TextParseException {
+	setUpTest_Ctor_6arg_doubles()
+	    when:
+		new GPOSRecord(m_n, DClass.IN, m_ttl, 90.001, m_lat, m_alt)
+	    then:
+	    thrown(IllegalArgumentException.class )
+	}
+
+	def "test_toosmall_latitude_Ctor_6arg_doubles"() throws TextParseException {
+	    setUpTest_Ctor_6arg_doubles()
+	    when:
+		new GPOSRecord(m_n, DClass.IN, m_ttl, m_long, -180.001, m_alt)
+	    then: 
+	    thrown(IllegalArgumentException.class)
+	}
+    
+    def "test_toobig_latitudesetUpTest_Ctor_6arg_doubles"() throws TextParseException {
+	setUpTest_Ctor_6arg_doubles()
+	    when:
+		new GPOSRecord(m_n, DClass.IN, m_ttl, m_long, 180.001, m_alt)
+	    then:
+	        thrown(IllegalArgumentException.class)
+	}
+
+    def "test_invalid_stringsetUpTest_Ctor_6arg_doubles"() {
+	setUpTest_Ctor_6arg_doubles()
+	    when:
+		new GPOSRecord(m_n, DClass.IN, m_ttl, new Double(m_long).toString(),
+			       "120.\\00ABC", new Double(m_alt).toString())
+	    then: thrown(IllegalArgumentException.class)
+     }	
+
+
+    
+    
+    
+    
+    protected void setUp_Ctor_6arg_Strings() throws TextParseException
 	{
 	    m_n = Name.fromString("The.Name.")
 	    m_ttl = 0xABCDL
@@ -160,12 +146,13 @@ public class GPOSRecordSpockTest extends Specification {
 	    m_alt = 100.101
 	}
 	
-	public void test_basic() throws TextParseException
-	{
+    def "test_basic_setUp_Ctor_6arg_Strings"() throws TextParseException {
+	setUp_Ctor_6arg_Strings()
 	    GPOSRecord gr = new GPOSRecord(m_n, DClass.IN, m_ttl,
 					   new Double(m_long).toString(),
 					   new Double(m_lat).toString(),
 					   new Double(m_alt).toString())
+	    expect:
 	    mgu.equals(m_n, gr.getName())
 	    mgu.equals(DClass.IN, gr.getDClass())
 	    mgu.equals(Type.GPOS, gr.getType())
@@ -177,56 +164,48 @@ public class GPOSRecordSpockTest extends Specification {
 	    mgu.equals(new Double(m_lat).toString(), gr.getLatitudeString())
 	    mgu.equals(new Double(m_alt).toString(), gr.getAltitudeString())
 	}
-
-	public void test_toosmall_longitude() throws TextParseException
-	{
-	    try {
+    
+    def "test_toosmall_longitude_setUp_Ctor_6arg_Strings"() throws TextParseException {
+	setUp_Ctor_6arg_Strings()
+	    when:
 		new GPOSRecord(m_n, DClass.IN, m_ttl,
 			       "-90.001", new Double(m_lat).toString(),
 			       new Double(m_alt).toString())
-		fail("IllegalArgumentException not thrown")
-	    }
-	    catch(IllegalArgumentException e){}
+	    then:
+	    thrown(IllegalArgumentException.class )
 	}
 
-	public void test_toobig_longitude() throws TextParseException
-	{
-	    try {
+    def "test_toobig_longitude_setUp_Ctor_6arg_Strings"() throws TextParseException {
+	    setUp_Ctor_6arg_Strings()
+	    when:
 		new GPOSRecord(m_n, DClass.IN, m_ttl,
 			       "90.001", new Double(m_lat).toString(),
 			       new Double(m_alt).toString())
-		fail("IllegalArgumentException not thrown")
-	    }
-	    catch(IllegalArgumentException e){}
-	}
+	    then:
+	    thrown(IllegalArgumentException.class )
+    }
 
-	public void test_toosmall_latitude() throws TextParseException
-	{
-	    try {
+    def "test_toosmall_latitude_setUp_Ctor_6arg_Strings"() throws TextParseException {
+	setUp_Ctor_6arg_Strings()
+	when:
 		new GPOSRecord(m_n, DClass.IN, m_ttl,
 			       new Double(m_long).toString(), "-180.001",
 			       new Double(m_alt).toString())
-		fail("IllegalArgumentException not thrown")
-	    }
-	    catch(IllegalArgumentException e){}
-	}
-
-	public void test_toobig_latitude() throws TextParseException
-	{
-	    try {
-		new GPOSRecord(m_n, DClass.IN, m_ttl,
-			       new Double(m_long).toString(), "180.001", new Double(m_alt).toString())
-		fail("IllegalArgumentException not thrown")
-	    }
-	    catch(IllegalArgumentException e){}
-	}
+	then:
+	  thrown(IllegalArgumentException.class )
     }
-    */
-    /*
-    public static class Test_rrFromWire extends TestCase
-    {
-	public void test_basic() throws IOException
-	{
+
+    def "test_toobig_latitude_setUp_Ctor_6arg_Strings"() throws TextParseException {
+	setUp_Ctor_6arg_Strings()
+	when:
+	new GPOSRecord(m_n, DClass.IN, m_ttl,
+			       new Double(m_long).toString(), "180.001", new Double(m_alt).toString())
+        then:
+	thrown(IllegalArgumentException.class)
+	
+    }
+   
+    def "test_basic_Test_rrFromWire"() throws IOException {
 	    def raw = [ 5, '-', '8', '.', '1', '2',
 		      6, '1', '2', '3', '.', '0', '7',
 		      3, '0', '.', '0' ].collect { entry -> (byte) entry }
@@ -234,146 +213,118 @@ public class GPOSRecordSpockTest extends Specification {
 	    
 	    GPOSRecord gr = new GPOSRecord()
 	    gr.rrFromWire(dnsInput)
+	    expect:
 	    mgu.equals(new Double(-8.12), new Double(gr.getLongitude()))
 	    mgu.equals(new Double(123.07), new Double(gr.getLatitude()))
 	    mgu.equals(new Double(0.0), new Double(gr.getAltitude()))
-	}
-	
-	public void test_longitude_toosmall() throws IOException
-	{
+    }
+    
+    def "test_longitude_toosmall_Test_rrFromWire"() throws IOException {
 	    def raw = [ 5, '-', '9', '5', '.', '0',
 				      6, '1', '2', '3', '.', '0', '7',
 				      3, '0', '.', '0' ].collect { entry -> (byte) entry }
 	    DNSInput dnsInput = new DNSInput(raw.toArray(new byte[raw.size()]))
 	    
 	    GPOSRecord gr = new GPOSRecord()
-	    try {
+	    when:
 		gr.rrFromWire(dnsInput)
-		fail("WireParseException not thrown")
-	    }
-	    catch(WireParseException e){}
-	}
+	    then: thrown(WireParseException.class )
+    }
 
-	public void test_longitude_toobig() throws IOException
-	{
+    def "test_longitude_toobig_Test_rrFromWire"() throws IOException {
 	    def raw = [ 5, '1', '8', '5', '.', '0',
 			6, '1', '2', '3', '.', '0', '7',
 			3, '0', '.', '0' ].collect { entry -> (byte) entry }
 	    DNSInput dnsInput = new DNSInput(raw.toArray(new byte[raw.size()]))
 
 	    GPOSRecord gr = new GPOSRecord()
-	    try {
+	    when:
 		gr.rrFromWire(dnsInput)
 		fail("WireParseException not thrown")
-	    }
-	    catch(WireParseException e){}
+	    then:
+	    thrown(WireParseException.class )
 	}
 
-	public void test_latitude_toosmall() throws IOException
-	{
+    def "test_latitude_toosmall_Test_rrFromWire"() throws IOException {
 	    def raw = [ 5, '-', '8', '5', '.', '0',
 			6, '-', '1', '9', '0', '.', '0',
 			3, '0', '.', '0' ].collect { entry -> (byte) entry }
 	    DNSInput dnsInput = new DNSInput(raw.toArray(new byte[raw.size()]))
 
 	    GPOSRecord gr = new GPOSRecord()
-	    try {
+	    when:
 		gr.rrFromWire(dnsInput)
-		fail("WireParseException not thrown")
-	    }
-	    catch(WireParseException e){}
-	}
-
-	public void test_latitude_toobig() throws IOException
-	{
+	    then: thrown(WireParseException.class )
+    }
+    
+    def "test_latitude_toobig_Test_rrFromWire"() throws IOException {
 	    def raw = [ 5, '-', '8', '5', '.', '0',
 			6, '2', '1', '9', '0', '.', '0',
 			3, '0', '.', '0' ].collect { entry -> (byte) entry }
 	    DNSInput dnsInput = new DNSInput(raw.toArray(new byte[raw.size()]))
 
 	    GPOSRecord gr = new GPOSRecord()
-	    try {
+	    when:
 		gr.rrFromWire(dnsInput)
-		fail("WireParseException not thrown")
-	    }
-	    catch(WireParseException e){}
-	}
+	    then: thrown(WireParseException.class )
     }
-    */
-    /*
-    public static class Test_rdataFromString extends TestCase
-    {
-	public void test_basic() throws IOException
-	{
+    
+    
+    def "test_basic_rdataFromString"() throws IOException {
 	    Tokenizer t = new Tokenizer("10.45 171.121212 1010787")
 	    
 	    GPOSRecord gr = new GPOSRecord()
 	    gr.rdataFromString(t, null)
+	    expect:
 	    mgu.equals(new Double(10.45), new Double(gr.getLongitude()))
 	    mgu.equals(new Double(171.121212), new Double(gr.getLatitude()))
 	    mgu.equals(new Double(1010787), new Double(gr.getAltitude()))
 	}
 
-	public void test_longitude_toosmall() throws IOException
-	{
+    def "test_longitude_toosmall_rdataFromString"() throws IOException {
 	    Tokenizer t = new Tokenizer("-100.390 171.121212 1010787")
 	    
 	    GPOSRecord gr = new GPOSRecord()
-	    try {
+	    when:
 		gr.rdataFromString(t, null)
-		fail("IOException not thrown")
-	    }
-	    catch(IOException e){}
-	}
+	    then: thrown(IOException.class )
+    }
 
-	public void test_longitude_toobig() throws IOException
-	{
+    def "test_longitude_toobig_rdataFromString"() throws IOException {
 	    Tokenizer t = new Tokenizer("90.00001 171.121212 1010787")
 	    
 	    GPOSRecord gr = new GPOSRecord()
-	    try {
+	    when:
 		gr.rdataFromString(t, null)
-		fail("IOException not thrown")
-	    }
-	    catch(IOException e){}
-	}
+	    then: thrown(IOException.class)
+    }
 
-	public void test_latitude_toosmall() throws IOException
-	{
+    def "test_latitude_toosmall_rdataFromString"() throws IOException {
 	    Tokenizer t = new Tokenizer("0.0 -180.01 1010787")
 	    
 	    GPOSRecord gr = new GPOSRecord()
-	    try {
+	    when:
 		gr.rdataFromString(t, null)
-		fail("IOException not thrown")
-	    }
-	    catch(IOException e){}
-	}
+	    then: thrown(IOException.class)
+    }
 
-	public void test_latitude_toobig() throws IOException
-	{
+    def "test_latitude_toobig_rdataFromString"() throws IOException {
 	    Tokenizer t = new Tokenizer("0.0 180.01 1010787")
 	    
 	    GPOSRecord gr = new GPOSRecord()
-	    try {
+	    when:
 		gr.rdataFromString(t, null)
-		fail("IOException not thrown")
-	    }
-	    catch(IOException e){}
-	}
+	    then: thrown(IOException.class )
+    }
 
-	public void test_invalid_string() throws IOException
-	{
+    def "test_invalid_string_rdataFromString"() throws IOException {
 	    Tokenizer t = new Tokenizer("1.0 2.0 \\435")
-	    try {
+	    when:
 		GPOSRecord gr = new GPOSRecord()
 		gr.rdataFromString(t, null)
-	    }
-	    catch(TextParseException e){}
-	}
+	    then:
+	    thrown(TextParseException.class)
     }
-    */
-// end of the classes
 
     def "test_rrToString"() throws TextParseException {
 	String exp = "\"10.45\" \"171.121212\" \"1010787.0\""
